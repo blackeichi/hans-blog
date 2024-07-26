29c255fdfdef479f5ff16f8803d8cab5afb08412
HanJeongWoo <87626378+blackeichi@users.noreply.github.com>
2024-07-26 23:44:52 +0900

    styiling

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,16 @@
+import styled from "styled-components";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps) => {
-  return <div>{children}</div>;
+  return <Backgorund>{children}</Backgorund>;
 };
+
+const Backgorund = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background-color: ${(props) => props.theme.bgGreen};
+`;
